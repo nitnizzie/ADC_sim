38f370cfd697008560472c287a33f766faa53284
HoonminLee <hunmin822@kaist.ac.kr>
2020-10-31 22:50:12 +0900

    code update 201031

--- python_unity.py
+++ python_unity.py
@@ -2,17 +2,17 @@ import numpy as np
 
 
 from mlagents_envs.environment import UnityEnvironment
-env = UnityEnvironment(file_name = 'Prototype 1')
+env = UnityEnvironment(file_name = 'Road3/Prototype 1')
 
 env.reset()
 behavior_name = list(env.behavior_specs)[0]
 decision_steps, _ = env.get_steps(behavior_name)
 cur_obs = decision_steps.obs[0][0,:]
 
-t = 0
-a1 = 0
-a2 = 150
-a3 = 150
+t1 = 0
+t2 = 0
+dg = 0
+act = 0
 
 for i in range(10000):
     decision_steps, terminal_steps = env.get_steps(behavior_name)
@@ -23,31 +23,63 @@ for i in range(10000):
     # s1 = cur_obs[6], s2 = cur_obs[7], s3 = cur_obs[8], s4 = cur_obs[9], s5 = cur_obs[10]
 
     # Set the actions
-    if (t == 0):
+
+    if (t1 == 0 and t2 == 0):
         env.set_actions(behavior_name, np.array([[0,150,150]]))
+        act = 0
 
-        if (cur_obs[9] > 15):
+        if (cur_obs[9] > 18):
             env.set_actions(behavior_name, np.array([[-0.3, 150, 150]]))
-        if (cur_obs[7] > 15):
+            act = 1
+        if (cur_obs[7] > 18):
             env.set_actions(behavior_name, np.array([[0.3, 150, 150]]))
+            act = 1
 
-        if (cur_obs[6] < cur_obs[8] and cur_obs[6] < 15):
+        if (cur_obs[6] < cur_obs[8] and cur_obs[6] < 7):
             env.set_actions(behavior_name, np.array([[-0.5, 150, 150]]))
-        if (cur_obs[6] > cur_obs[8] and cur_obs[8] < 15):
+            act = 2
+        if (cur_obs[6] > cur_obs[8] and cur_obs[8] < 7):
             env.set_actions(behavior_name, np.array([[0.5, 150, 150]]))
+            act = 2
 
-        if (cur_obs[7] < 5):
-            env.set_actions(behavior_name, np.array([[-0.5, 150, 150]]))
-            t = 3
-            a1 = -0.5
-        if (cur_obs[9] < 5):
-            env.set_actions(behavior_name, np.array([[0.5, 150, 150]]))
-            t = 3
-            a1 = 0.5
+        if (cur_obs[7] < 4):
+            env.set_actions(behavior_name, np.array([[-0.3, 150, 150]]))
+            t1 = 3
+            t2 = 3
+            dg = -0.3
+            act = 3
+        if (cur_obs[9] < 4):
+            env.set_actions(behavior_name, np.array([[0.3, 150, 150]]))
+            t1 = 3
+            t2 = 3
+            dg = 0.3
+            act = 3
 
-    else :
-        t = t - 1
-        env.set_actions(behavior_name, np.array([[a1, a2, a3]]))
+        if ((cur_obs[6] < 8 or cur_obs[8] < 8 or cur_obs[10] < 8)
+                and cur_obs[6] + cur_obs[7] > cur_obs[8] + cur_obs[9]):
+            env.set_actions(behavior_name, np.array([[0.7, 150, 150]]))
+            t1 = 5
+            t2 = 5
+            dg = 0.7
+            act = 4
+        if ((cur_obs[6] < 8 or cur_obs[8] < 8 or cur_obs[10] < 8)
+                and cur_obs[6] + cur_obs[7] < cur_obs[8] + cur_obs[9]):
+            env.set_actions(behavior_name, np.array([[-0.7, 150, 150]]))
+            t1 = 5
+            t2 = 5
+            dg = -0.7
+            act = 4
+
+    if (t1 == 0 and t2 != 0):
+        t2 = t2 - 1
+        act = 5
+        env.set_actions(behavior_name, np.array([[0, 150, 150]]))
+
+    if (t1 != 0):
+        t1 = t1 - 1
+        env.set_actions(behavior_name, np.array([[dg, 150, 150]]))
+
+    print("Action : ", act, "   Range : ", i)
 
     # Move the simulation forward
     env.step()
